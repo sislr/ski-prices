150f7b5e61cf0cbb538eae8cce0eac0385e25bc8
Simon Isler <mail@simonisler.ch>
2025-11-01 11:23:48 +0100

    Lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   namespace :admin do
-      resources :price_entries
-      resources :ski_passes
-      resources :ski_resorts
-      resources :ski_seasons
+    resources :price_entries
+    resources :ski_passes
+    resources :ski_resorts
+    resources :ski_seasons
 
-      root to: "ski_resorts#index"
-    end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    root to: "ski_resorts#index"
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
