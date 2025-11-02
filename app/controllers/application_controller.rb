814b3fbf8095584cbe8cb869c9ebc60fc43e369e
Simon Isler <mail@simonisler.ch>
2025-11-02 15:56:56 +0100

    Protect app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  http_basic_authenticate_with name: Rails.application.credentials.basic_auth.user,
+                               password: Rails.application.credentials.basic_auth.password,
+                               if: -> { Rails.env.production? }
 end
