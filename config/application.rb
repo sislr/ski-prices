4d243d20ae8bc96257cb816b028224ab4462cebd
Simon Isler <mail@simonisler.ch>
2025-11-02 20:29:17 +0100

    Disable basic auth for jobs

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,7 @@ module SkiPrices
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.mission_control.jobs.http_basic_auth_enabled = false
   end
 end
