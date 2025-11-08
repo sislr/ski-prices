1cf668811819430dcfbf1bd963a737a435cbb02e
Simon Isler <mail@simonisler.ch>
2025-11-08 13:59:51 +0100

    Fix

--- config/initializers/mission_control_auth.rb
+++ config/initializers/mission_control_auth.rb
@@ -1,2 +1,4 @@
+return if ENV.fetch("SECRET_KEY_BASE_DUMMY", nil).present?
+
 MissionControl::Jobs.http_basic_auth_user = Rails.application.credentials.admin.basic_auth.user
 MissionControl::Jobs.http_basic_auth_password = Rails.application.credentials.admin.basic_auth.password
