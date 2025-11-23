52b09aba49a99e9962983385764d427851c78914
Simon Isler <mail@simonisler.ch>
2025-11-23 11:47:20 +0100

    Validate price entry date

--- app/models/price_entry.rb
+++ app/models/price_entry.rb
@@ -4,4 +4,13 @@ class PriceEntry < ApplicationRecord
   scope :ordered_by_date, -> { order(created_at: :asc) }
 
   validates :price_in_chf, presence: true, numericality: { greater_than: 0 }
+  validate :ski_pass_date_is_not_in_past
+
+  private
+
+  def ski_pass_date_is_not_in_past
+    return unless ski_pass.valid_on.past?
+
+    errors.add(:base, "Cannot create price entry for past dates")
+  end
 end
--- test/models/price_entry_test.rb
+++ test/models/price_entry_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class PriceEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "price entry cannot be created for date in the past" do
+    pass = ski_passes(:st_moritz_christmas_day_pass_adult)
+    pass.valid_on = 1.day.ago
+
+    price_entry = PriceEntry.new(ski_pass: pass, price_in_chf: 100)
+    assert price_entry.invalid?
+  end
 end
