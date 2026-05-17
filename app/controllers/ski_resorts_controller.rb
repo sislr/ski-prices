650bea77e29925d9728d94da8011a8288ff36f37
Simon Isler <mail@simonisler.ch>
2026-05-17 11:25:27 +0200

    Improve layout of past seasons

--- app/controllers/ski_resorts_controller.rb
+++ app/controllers/ski_resorts_controller.rb
@@ -6,10 +6,10 @@ class SkiResortsController < ApplicationController
       .order("ski_resorts.name ASC, ski_seasons.start_date ASC")
       .map { |season| [ season.ski_resort, season ] }
 
-    @past_seasons_by_resort = SkiSeason.past
+    @past_seasons_by_year = SkiSeason.past
       .includes(:ski_resort)
       .joins(:ski_resort)
-      .order("ski_resorts.name ASC, ski_seasons.end_date DESC")
-      .map { |season| [ season.ski_resort, season ] }
+      .order("ski_seasons.end_date DESC, ski_resorts.name ASC")
+      .group_by { |season| "#{season.start_date.year}/#{season.end_date.year.to_s[-2..]}" }
   end
 end
